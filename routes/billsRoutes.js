933e7df962a1e91c0895ace24ccbe46fb9ec5444
Kabilesh31 <kabigoarge@gmail.com>
2026-03-02 15:30:55 +0530

    sat fixed

--- routes/billsRoutes.js
+++ routes/billsRoutes.js
@@ -1,31 +1,26 @@
-const express = require("express");
-const router = express.Router();
-const upload = require("../middleware/multer");
+    const express = require("express");
+    const router = express.Router();
 
-const {
-  createBill,
-  getBills,
-  changeOrderStatus,
-  getBillById,   
-  updatePaymentStatus,
-  markHasDelivered,
-  getBillsByStaff,
-  getBillsByDeliveryStaff
-} = require("../controllers/billsController");
+    const {
+      createBill,
+      getBills,
+      changeOrderStatus,
+        getBillById,   
+      updatePaymentStatus,
+      markHasDelivered
+    } = require("../controllers/billsController");
 
-// POST create new bill
-router.post("/", createBill);
+    // POST create new bill
+    router.post("/", createBill);
 
-// GET all bills (optionally filtered by customerId)
-router.get("/", getBills);
+    // GET all bills (optionally filtered by customerId)
+    router.get("/", getBills);
 
-router.get("/:billId", getBillById);
-// PATCH change order status
-router.patch("/:billId/status", changeOrderStatus);
-router.put("/delivered/:id", upload.single("deliveryImage"), markHasDelivered );
+    router.get("/:billId", getBillById);
+    // PATCH change order status
+    router.patch("/:billId/status", changeOrderStatus);
+    router.put("/delivered/:id", markHasDelivered );
 
-router.patch("/:id/payment", updatePaymentStatus);
-router.get("/getBillsByStaff/:id", getBillsByStaff)
-router.get("/getBillsByDeliveryStaff/:id", getBillsByDeliveryStaff)
+    router.patch("/:id/payment", updatePaymentStatus);
 
-module.exports = router;
+    module.exports = router;
